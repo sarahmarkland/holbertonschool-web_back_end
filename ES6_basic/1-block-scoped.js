e06b3880c31027c9cad1ce54e12f5b4f5c547835
sarahmarkland <sarah.markland@icloud.com>
2023-09-07 16:48:46 +0000

    task 1 output fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task3 = true;
+    const task4 = false;
   }
 
   return [task, task2];
