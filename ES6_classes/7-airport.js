b77d11aa0984025cf1e77e7671b8e5f25d37828f
sarahmarkland <sarah.markland@icloud.com>
2023-09-09 21:46:13 +0000

    task 7 lil b

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -2,14 +2,12 @@
 // allows other modules to import it
 export default class Airport {
   constructor(name, code) {
-    // underscores before property names is a common convention to
-    // indicate that these properties are intended to be private and not
-    // accessed directly from outside the class
+
     this._name = name;
     this._code = code;
   }
 
   toString() {
-    return `${this._name} (${this._code})`;
+    return `[object ${this._code}]`;
   }
 }
